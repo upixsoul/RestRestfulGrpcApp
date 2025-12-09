3f5651b84a8c9d623b967dd43cfe6d21222897c6
David Alejandro García Garcia <AL07106842@tecmilenio.mx>
2025-12-08 22:55:21 -0600

    add grpc

---
+++ ConsoleAppGrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+
+namespace ConsoleAppGrpcClient;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var channel = GrpcChannel.ForAddress("http://localhost:5169");
+
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(new HelloRequest
+        {
+            Name = "David"
+        });
+
+        Console.WriteLine("Respuesta del servidor: " + reply.Message);
+    }
+}
